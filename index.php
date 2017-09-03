68fccd8309aa47a220d4875d0026af3f6324b361
ilwp104 <will4981@naver.com>
2017-09-04 00:35:42 +0900

    검출농약 width 변경

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
           <td style="width : 60px">인증사항</td>
           <td style="width : 60px">검사기관</td>
           <td style="width : 80px">시료채취일</td>
-          <td style="width : 60px">검출농약</td>
+          <td style="width : 80px">검출농약</td>
           <td style="width : 50px">검출양<br />(mg/kg)</td>
           <td style="width : 50px">기준치<br />(mg/kg)</td>
           <td>난각코드</td>
