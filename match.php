e60fe84a5dbd4b1949138bf2968091070f7838d3
ilwp104 <will4981@naver.com>
2017-09-04 00:35:26 +0900

    스크롤 시 내려오는 기능 추가

--- match.php
+++ match.php
@@ -61,7 +61,7 @@
         <td style='width : 60px'>인증사항</td>
         <td style='width : 60px'>검사기관</td>
         <td style='width : 80px'>시료채취일</td>
-        <td style='width : 60px'>검출농약</td>
+        <td style='width : 80px'>검출농약</td>
         <td style='width : 50px'>검출양<br />(mg/kg)</td>
         <td style='width : 50px'>기준치<br />(mg/kg)</td>
         <td>난각코드</td>
@@ -91,5 +91,22 @@
 
       ?>
     </article>
+    <script type="text/javascript">
+      $(window).scroll(function() {
+         if ($(window).scrollTop() > 150) {
+           $("header").css("position","fixed");
+           $("header").css("padding-top","150px");
+           $("header").css("top","-150px");
+           $("header").css("transition","padding .5s");
+
+         }
+         else {
+           $("header").css("position","absolute");
+           $("header").css("padding-top","0");
+           $("header").css("top","0");
+           $("header").css("transition","padding .3s");
+         }
+       });
+    </script>
   </body>
 </html>
