9e6877435b0eeb74c6d678ef59eeac3edfa922ff
ilwp104 <will4981@naver.com>
2017-09-04 00:30:16 +0900

    match에 전체보기 기능 추가

--- match.php
+++ match.php
@@ -5,78 +5,91 @@
     <title></title>
   </head>
   <body>
-    <form class="" action="match.php" method="post">
-      <input type="text" name="string" value="">
-      <select class="" name="matchSelect">
-        <option value="select">전체보기</option>
-        <option value="난각코드">난각코드</option>
-        <option value="농가명">농가명</option>
-        <option value="주소">주소</option>
-      </select>
-      <input type="submit" name="" value="검색">
-    </form>
+    <link rel="stylesheet" href="./css/main.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+
     <?php
+    require_once("/head.html");
+
     require_once("/DB/DBinfo.php");
 
     $string = $_POST["string"];
     $match = $_POST["matchSelect"];
-    echo "$string<br />";
 
     $connect = mysql_connect($hostname,$username,$passwd) or die("connect 실패");
     mysql_select_db($dbname,$connect) or die("db연결 실패");
-
-    if ($match=="난각코드") {
+    if ($match=="select") {
+      echo "<script>
+      alert('선택해주세요');
+      </script>";
+      $sql = "select * from EggCheck";
+      $result = mysql_query($sql,$connect)or die("sql 실패");
+    }
+    elseif ($match=="전체보기") {
+      $sql = "select * from EggCheck";
+      $result = mysql_query($sql,$connect)or die("sql 실패");
+    }
+    elseif ($match=="난각코드") {
       $sql = "select * from EggCheck where code like \"%{$string}%\"";
       $result = mysql_query($sql,$connect)or die("sql 실패");
-      // echo "select * from EggCheck where code like '%{$string}%'";
     }
     elseif ($match=="농가명") {
       $sql = "select * from EggCheck where name like \"%{$string}%\"";
       $result = mysql_query($sql,$connect)or die("sql 실패");
-      // echo "select * from EggCheck where name like '%{$string}%'";
     }
     elseif ($match=="주소") {
       $sql = "select * from EggCheck where address like \"%{$string}%\"";
       $result = mysql_query($sql,$connect)or die("sql 실패");
-    }
-    echo "<table>";
-    echo "<tr>
-    <td>연번</td>
-    <td>시도</td>
-    <td>농가명</td>
-    <td>주소</td>
-    <td>사육규모<br />(수)</td>
-    <td>생산량</td>
-    <td>인증사항</td>
-    <td>검사기관</td>
-    <td>시료 채취일</td>
-    <td>검출농약</td>
-    <td>검출양<br />(mg/kg)</td>
-    <td>기준치<br />(mg/kg)</td>
-    <td>난각코드</td>
-    </tr>";
-    while ($row=mysql_fetch_array($result)) {
-      echo "<tr>";
-      echo "
-      <td>{$row[num]}</td>
-      <td>{$row[city]}</td>
-      <td>{$row[name]}</td>
-      <td>{$row[address]}</td>
-      <td>{$row[scale]}</td>
-      <td>{$row[product]}</td>
-      <td>{$row[certifications]}</td>
-      <td>{$row[InspectionAgency]}</td>
-      <td>{$row[collectionDay]}</td>
-      <td>{$row[nongyak]}</td>
-      <td>{$row[nongAmount]}</td>
-      <td>{$row[nongStandard]}</td>
-      <td>{$row[code]}</td>";
-      echo "</tr>";
-    }
-    echo "</table>";
+    }?>
+    <article class="wrapTable">
 
-    mysql_close();
+      <?php
 
-    ?>
+      $num = mysql_num_rows($result);
+      if ($num == 0 ) {
+        echo "<p style='text-align : center'>확인된 계란이 없습니다.</p>";
+      }
+      else {
+        echo "<table>";
+        echo "<tr>
+        <td style='width : 30px'>연번</td>
+        <td style='width : 30px'>시도</td>
+        <td>농가명</td>
+        <td>주소</td>
+        <td style='width : 60px'>사육규모<br />(수)</td>
+        <td style='width : 60px'>생산량</td>
+        <td style='width : 60px'>인증사항</td>
+        <td style='width : 60px'>검사기관</td>
+        <td style='width : 80px'>시료채취일</td>
+        <td style='width : 60px'>검출농약</td>
+        <td style='width : 50px'>검출양<br />(mg/kg)</td>
+        <td style='width : 50px'>기준치<br />(mg/kg)</td>
+        <td>난각코드</td>
+        </tr>";
+        while ($row=mysql_fetch_array($result)) {
+          echo "<tr>";
+          echo "
+          <td>{$row[num]}</td>
+          <td>{$row[city]}</td>
+          <td>{$row[name]}</td>
+          <td>{$row[address]}</td>
+          <td>{$row[scale]}</td>
+          <td>{$row[product]}</td>
+          <td>{$row[certifications]}</td>
+          <td>{$row[InspectionAgency]}</td>
+          <td>{$row[collectionDay]}</td>
+          <td>{$row[nongyak]}</td>
+          <td>{$row[nongAmount]}</td>
+          <td>{$row[nongStandard]}</td>
+          <td>{$row[code]}</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+      }
+
+      mysql_close();
+
+      ?>
+    </article>
   </body>
 </html>
