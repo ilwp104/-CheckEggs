2a7daef740cccdedb0622f5564e4ad27d0c6ca5f
ilwp104 <will4981@naver.com>
2017-09-07 13:26:00 +0900

    백업

---
+++ backup/indexback.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>EggCheck</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link rel="apple-touch-icon" sizes="57x57" href="./favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+  </head>
+  <body>
+    <link rel="stylesheet" href="./css/main.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+
+  <?php
+    include("./head.html");
+  ?>
+
+
+
+    <article class="wrapTable">
+      <table>
+        <tr>
+          <td style="width : 30px">연번</td>
+          <td style="width : 30px">시도</td>
+          <td>농가명</td>
+          <td>주소</td>
+          <td style="width : 60px">사육 규모<br />(수)</td>
+          <td style="width : 60px">생산량</td>
+          <td style="width : 60px">인증 사항</td>
+          <td style="width : 60px">검사 기관</td>
+          <td style="width : 80px">시료 채취일</td>
+          <td style="width : 80px">검출 농약</td>
+          <td style="width : 50px">검출양<br />(mg/kg)</td>
+          <td style="width : 50px">기준치<br />(mg/kg)</td>
+          <td>난각코드</td>
+        </tr>
+
+          <?php
+            include("./DB/DBinfo.php");
+        		$connect = mysql_connect($hostname,$username,$passwd) or die("connect 실패");
+        		$db_con=mysql_select_db($dbname,$connect)or die("db연결 실패");
+        		$sql = "select * from EggCheck";
+        		$result = mysql_query($sql,$connect) or die("sql 실패");
+            mysql_query("SET NAMES utf8",$connect);
+        		while ($row=mysql_fetch_array($result)) {
+              echo "<tr>";
+          			echo "
+                <td>{$row[num]}</td>
+                <td>{$row[city]}</td>
+                <td>{$row[name]}</td>
+                <td>{$row[address]}</td>
+                <td>{$row[scale]}</td>
+                <td>{$row[product]}</td>
+                <td>{$row[certifications]}</td>
+                <td>{$row[InspectionAgency]}</td>
+                <td>{$row[collectionDay]}</td>
+                <td>{$row[nongyak]}</td>
+                <td>{$row[nongAmount]}</td>
+                <td>{$row[nongStandard]}</td>
+                <td>{$row[code]}</td>";
+              echo "</tr>";
+            }
+        		mysql_close($connect);
+          ?>
+        </tr>
+      </table>
+
+    </article>
+
+    <script type="text/javascript">
+    var headerSize;
+    if (screen.availWidth < 800) {
+      headerSize=160;
+    }
+    else{
+      headerSize=200;
+    }
+      $(window).scroll(function() {
+        if ($(window).scrollTop() <= headerSize) {
+          if (screen.availWidth < 800) {
+            // $("header").css("height","160px");
+            // $(".head").css("font-size","30px");
+          }
+          else{
+            // $("header").css("height","200px");
+            // $(".head").css("font-size","50px");
+          }
+          // $("header").css("position","absolute");
+          // $("header").css("margin-top","0");
+          // $("header").css("top","0");
+          // $("header").css("transition","margin .0s");
+        }
+        else if ($(window).scrollTop() <= 400 && $(window).scrollTop() >  headerSize+50) {
+          // $("header").css("margin-top","0px");
+          // $("header").css("transition","margin .5s");
+        }
+         else if ($(window).scrollTop() > 400) {
+          //  $("header").css("height","150px");
+          //  $(".head").css("font-size","20px");
+          //  $("header").css("position","fixed");
+          //  $("header").css("margin-top","350px");
+          //  $("header").css("top","-350px");
+          //  $("header").css("transition","margin 0s");
+         }
+       });
+    </script>
+  </body>
+</html>
---
+++ backup/matchback.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <link rel="stylesheet" href="./css/main.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+
+    <?php
+    include("./head.html");
+
+    include("./DB/DBinfo.php");
+
+    $string = $_POST["string"];
+    $match = $_POST["matchSelect"];
+
+    $connect = mysql_connect($hostname,$username,$passwd) or die("connect 실패");
+    mysql_select_db($dbname,$connect) or die("db연결 실패");
+    if ($match=="select") {
+      echo "<script>
+      alert('선택해주세요');
+      </script>";
+      $sql = "select * from EggCheck";
+      $result = mysql_query($sql,$connect)or die("sql 실패");
+    }
+    elseif ($match=="전체보기") {
+      $sql = "select * from EggCheck";
+      $result = mysql_query($sql,$connect)or die("sql 실패");
+    }
+    elseif ($match=="난각코드") {
+      $sql = "select * from EggCheck where code like \"%{$string}%\"";
+      $result = mysql_query($sql,$connect)or die("sql 실패");
+    }
+    elseif ($match=="농가명") {
+      $sql = "select * from EggCheck where name like \"%{$string}%\"";
+      $result = mysql_query($sql,$connect)or die("sql 실패");
+    }
+    elseif ($match=="주소") {
+      $sql = "select * from EggCheck where address like \"%{$string}%\"";
+      $result = mysql_query($sql,$connect)or die("sql 실패");
+    }?>
+    <article class="wrapTable">
+
+      <?php
+
+      $num = mysql_num_rows($result);
+      if ($num == 0 ) {
+        echo "<p style='text-align : center'>확인된 계란이 없습니다.</p>";
+      }
+      else {
+        echo "<table>";
+        echo "<tr>
+        <td style='width : 30px'>연번</td>
+        <td style='width : 30px'>시도</td>
+        <td>농가명</td>
+        <td>주소</td>
+        <td style='width : 60px'>사육규모<br />(수)</td>
+        <td style='width : 60px'>생산량</td>
+        <td style='width : 60px'>인증사항</td>
+        <td style='width : 60px'>검사기관</td>
+        <td style='width : 80px'>시료채취일</td>
+        <td style='width : 80px'>검출농약</td>
+        <td style='width : 50px'>검출양<br />(mg/kg)</td>
+        <td style='width : 50px'>기준치<br />(mg/kg)</td>
+        <td>난각코드</td>
+        </tr>";
+        while ($row=mysql_fetch_array($result)) {
+          echo "<tr>";
+          echo "
+          <td>{$row[num]}</td>
+          <td>{$row[city]}</td>
+          <td>{$row[name]}</td>
+          <td>{$row[address]}</td>
+          <td>{$row[scale]}</td>
+          <td>{$row[product]}</td>
+          <td>{$row[certifications]}</td>
+          <td>{$row[InspectionAgency]}</td>
+          <td>{$row[collectionDay]}</td>
+          <td>{$row[nongyak]}</td>
+          <td>{$row[nongAmount]}</td>
+          <td>{$row[nongStandard]}</td>
+          <td>{$row[code]}</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+      }
+
+      mysql_close();
+
+      ?>
+    </article>
+    <script type="text/javascript">
+      $(window).scroll(function() {
+         if ($(window).scrollTop() > 150) {
+           $("header").css("position","fixed");
+           $("header").css("padding-top","150px");
+           $("header").css("top","-150px");
+           $("header").css("transition","padding .5s");
+
+         }
+         else {
+           $("header").css("position","absolute");
+           $("header").css("padding-top","0");
+           $("header").css("top","0");
+           $("header").css("transition","padding .3s");
+         }
+       });
+    </script>
+  </body>
+</html>
